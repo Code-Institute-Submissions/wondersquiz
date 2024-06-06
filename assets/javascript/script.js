5e89861812f7ab70cf241a699921e76e0093c767
Regan Boreland <reganboreland@hotmail.co.uk>
2024-06-06 23:20:34 +0000

    name base functions to run game

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,3 +7,39 @@ const quizContainer = document.getElementById("quizes_container");
 const qaContainer = document.getElementById("qa_container");
 const scoreContainer = document.getElementById("score_container");
 
+//Onload function
+
+//function to display username
+function displayUsername(){
+
+}
+
+//function to submit username
+function submitUsername(){
+
+}
+
+//ancient wonders quiz
+function ancientWonders(){
+
+}
+
+//modern wonders quiz
+function modernWonders(){
+
+}
+
+//increment correct score function
+function correctScore(){
+
+}
+
+//increment incorrect score
+function incorrectScore(){
+
+}
+
+// display final score
+function displayScore(){
+
+}
