7ede8577ffc56e35a3982797776c3485df6b6633
Regan Boreland <reganboreland@hotmail.co.uk>
2024-06-08 14:16:40 +0000

    create array of questions and answers for ancient wonders quiz

---
+++ assets/javascript/questions.js
@@ -0,0 +1,56 @@
+let ancientQuestions = [
+    {
+        question: "Which one of these structures is not apart of the wonders of the ancient world?",
+        options: ["The Pyramids of Giza", "The Hanging Gardens of Babylon", "The Temple of Artemis"],
+        answer: "The Great Wall of China"
+    },
+    {
+        question: "Which one of these structures represents the King of the Greek Gods?",
+        options: ["Statue of Zues", "Lighthouse of Alexandria", "Colossus of Rhoades", "Mausolem at Halicarnassus"],
+        answer: "Statue of Zues"
+    },
+    {
+        question: "Which one of these structures is located in Turkey?",
+        options: ["Mausolem at Halicarnassus", "Lighthouse of Alexandria", "Hanging Gardens of Babylon", "Pyramids of Giza"],
+        answer:"Mausolem at Halicarnassus"
+    },
+    {
+        question: "What century was the Colossus of Rhoades built?",
+        options: ["2nd Century A.D.", "4th Century B.C.", "20th Century", "10th Century A.D."],
+        answer: "4th Century B.C."
+    },
+    {
+        question: "The seven wonders of the ancient world surround which sea?",
+        options: ["Atlantic", "Pacific", "The Gulf of Mexico", "The Mediterranian"],
+        answer: "The Mediterranian"
+    }
+]
+
+let modernQuestions = [
+    {
+        question: "",
+        options: [],
+        answer: ""
+    },
+    {
+        question: "",
+        options: [],
+        answer: ""
+    },
+    {
+        question: "",
+        options: [],
+        answer: ""
+    },
+    {
+        question: "",
+        options: [],
+        answer: ""
+    },
+    {
+        question: "",
+        options: [],
+        answer: ""
+    },
+
+]
