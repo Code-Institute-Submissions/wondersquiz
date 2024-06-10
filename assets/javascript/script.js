0e53e6baae42857242ea8e463570334afcc2ea44
Regan Boreland <reganboreland@hotmail.co.uk>
2024-06-10 15:07:08 +0000

    add get new question function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -57,127 +57,131 @@ function submitUsername(){
  */
 
 //quiz questions
-let quizQuestions = [
+const questions = [
     {
         question: "Which one of these structures is not apart of the wonders of the ancient world?",
-        option1 : "The Pyramids of Giza",
-        option2: "The Hanging Gardens of Babylon",
-        option3: "The Temple of Artemis",
-        option4: "The Great Wall of China",
+        choice1: "The Pyramids of Giza",
+        choice2: "The Hanging Gardens of Babylon",
+        choice3: "The Temple of Artemis",
+        choice4: "The Great Wall of China",
         answer: 4
     },
     {
         question: "Which one of these structures represents the King of the Greek Gods?",
-        option1: "The Statue of Zues",
-        option2: "Lighthouse of Alexandria",
-        option3: "Colossus of Rhoades",
-        option4: "Mausolem at Halicarnassus",
+        choice1: "The Statue of Zues",
+        choice2: "Lighthouse of Alexandria",
+        choice3: "Colossus of Rhoades",
+        choice4: "Mausolem at Halicarnassus",
         answer: 1
     },
     {
         question: "Which one of these structures is located in Turkey?",
-        option1: "Mausolem at Halicarnassus",
-        option2: "Lighthouse of Alexandria",
-        option3: "Hanging Gardens of Babylon",
-        option4: "Pyramids of Giza",
+        choice1: "Mausolem at Halicarnassus",
+        choice2: "Lighthouse of Alexandria",
+        choice3: "Hanging Gardens of Babylon",
+        choice4: "Pyramids of Giza",
         answer: 1
     },
     {
         question: "What century was the Colossus of Rhoades built?",
-        option1: "2nd Century A.D.",
-        option2: "4th Century B.C.",
-        option3: "20th Century",
-        option4: "10th Century A.D.",
+        choice1: "2nd Centure A.D.",
+        choice2: "4th Century B.C.",
+        choice3: "20th Century",
+        choice4: "10th Century A.D.",
         answer: 2
     },
     {
         question: "The seven wonders of the ancient world surround which sea?",
-        option1: "Atlantic",
-        option2: "Pacific",
-        option3: "The Gulf of Mexico",
-        option4: "The Mediterranian",
+        choice1: "Atlantic",
+        choice2: "Pacific",
+        choice3: "The Gulf of Mexico",
+        choice4: "The Mediterranian",
         answer: 4
     },
     {
         question: "Which one of these modern wonders is located in Brazil?",
-        option1: "Taj Mahal",
-        option2: "Christ the Redeemer",
-        option3: "Petra",
-        option4: "Chichen Itza",
+        choice1: "Taj Mahal",
+        choice2: "Christ the Redeemer",
+        choice3: "Petra",
+        choice4: "Chichen Itza",
         answer: 2
     },
     {
         question: "One of these structures was built in 80 A.D., which one was it?",
-        option1: "Colosseum",
-        option2: "Great Wall of China",
-        option3: "Machu Picchu",
-        option4: "Taj Mahal",
+        choice1: "Colosseum",
+        choice2: "Great Wall of China",
+        choice3: "Machu Picchu",
+        choice4: "Taj Mahal",
         answer: 1
     },
     {
         question: "Which one of these structures was built for the architects deceased wife?",
-        option1: "Taj Mahal",
-        option2: "Colossus of Rhoades",
-        option3: "Christ the Redeemer",
-        option4: "Lighthouse of Alexandria",
+        choice1: "Taj Mahal",
+        choice2: "Colossus of Rhoades",
+        choice3: "Christ the redeemer",
+        choice4: "Lighthouse of Alexandria",
         answer: 1
     },
 ]
 
-//variables to keep score and count questions
-let correctScore
-let incorrectScore
-let currentQuestion
-let questionCounter
-
-let avaiableQuestions
+//default variables for tracking score, question number and random questions
+let correctScore; 
+let incorrectScore; 
+let currentQuestion = {}; 
+let questionCounter; 
+let avaiableQuestions = [];
 
 //variables to display scores, questions and option buttons
 const correctScoreCounter = document.getElementById("cScore");
 const incorrectScoreCounter = document.getElementById("iScore");
-const questionContent = document.getElementById("questionsContent");
-
-//the maximum amount of questions thats available to the user
-const MAX_QUESTIONS = 8;
-
-//creates an array from option elements with option_bttn class name
-const optionBtnns = Array.from(document.getElementsByClassName("option_bttn"));
+const questionContent = document.getElementById("questions_Content");
+const startBttn = document.getElementById("startBttn");
+const questionCounterText = document.getElementById("question_counter_text");
+const answerBttn = document.getElementById("answers_container"); 
+const nextBttn = document.getElementById("next_bttn")
 
 const qaContainer = document.getElementById("qa_container");
 const scoreContainer = document.getElementById("score_container");
 qaContainer.style.display = "none";
 scoreContainer.style.display = "none";
+//creates array from option buttons
+const options = Array.from(document.getElementsByClassName("option_bttn"));
+//maximum questions in the quiz
+const maxQuestions = 8;
 
 //function to start quiz
 function startQuiz(){
-    let startBttn = document.getElementById("startBttn");
-    
-//hides pre existing variables
-    startBttn.style.display = "none";
-    usernameContainer.style.display = "none";
-    greetContainer.style.display = "none";
-
-//reveals question and score container
+    //reveals score and questions containers
     qaContainer.style.display = "block flex";
     scoreContainer.style.display = "block flex";
+    //removes start button and greeting container
+    startBttn.style.display = "none";
+    greetContainer.style.display = "none";
 
-//Initial counters for score and questions
+    //sets values for below variables
     questionCounter = 0;
     correctScore = 0;
     incorrectScore = 0;
-/**
- * sets the number allocated with correctScore and incorrectScore
- * to the elements with the ids of correctScoreCounter and incorrectScoreCounter
- */
+    avaiableQuestions = [...questions]
+
+    nextBttn.innerHTML ="Submit Answer";
+
     correctScoreCounter.innerHTML = correctScore;
     incorrectScoreCounter.innerHTML = incorrectScore;
-//uses the quizQuestions array for the avaiableQuestions array
-    availableQuestions = [...quizQuestions];
 
-//calls the fetchQuestion function
-    fetchQuestion();
+    getNewQuestion();
 }
 
-function fetchQuestion(){
+//displays question in element with question_Content id
+function getNewQuestion() {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * avaiableQuestions.length);
+    currentQuestion = avaiableQuestions[questionIndex];
+    questionContent.innerText = currentQuestion.question
+
+}
+
+
+function checkAnswer(){
 
 }
